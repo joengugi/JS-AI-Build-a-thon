4a8d29ee743360a0475e98aa2bb6835cc4e32c79
Joengugi <joengugi361@gmail.com>
2025-06-11 12:01:17 +0300

    {commit_message}

---
+++ sample2.js
@@ -0,0 +1,39 @@
+import OpenAI from "openai";
+
+const token = process.env["GITHUB_TOKEN"];
+const endpoint = "https://models.github.ai/inference";
+const modelName = "openai/gpt-4o";
+
+export async function main() {
+
+  const client = new OpenAI({ baseURL: endpoint, apiKey: token });
+
+  const stream = await client.chat.completions.create({
+    messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: "Give me 5 good reasons why I should exercise every day." },
+      ],
+      model: modelName,
+      stream: true,
+      stream_options: {include_usage: true}
+    });
+
+    var usage = null;
+    for await (const part of stream) {
+      process.stdout.write(part.choices[0]?.delta?.content || '');
+	  if (part.usage){
+		usage = part.usage;
+      }
+    }
+    process.stdout.write('\n');
+    
+    if (usage) {
+	  process.stdout.write(`Prompt tokens: ${usage.prompt_tokens}\n`);
+	  process.stdout.write(`Completion tokens: ${usage.completion_tokens}\n`);
+	  process.stdout.write(`Total tokens: ${usage.total_tokens}\n`);
+    }
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
